adf3707b10bef5c8c3c867026942b27dd391a2e1
zhangkaicheng <378925946@qq.com>
2017-10-09 10:04:59 +0800

    周考1

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
-    gulpUglify = require('gulp-uglify');
-    gulp.task('jsmin',function(){
-        gulp.src('./*.js').pipe(gulpUglify()).pipe(gulp.dest('./dist/js'));
-    })
+    gulpUglify = require('gulp-uglify'),
+    gulpConcat = require('gulp-concat');
+    // gulp.task('jsmin',function(){
+    //     gulp.src('./*.js').pipe(gulpUglify()).pipe(gulp.dest('./dist/js'));
+    // })
 
     gulp.task('jshb',function(){
-        gulp.src('./dist/js/*.js').pipe(gulp.dest('./dist/js/hb.js'));
+        gulp.src('./*.js').pipe(gulpUglify()).pipe(gulpConcat('all.min.js')).pipe(gulp.dest('./dist/js'));
     })
